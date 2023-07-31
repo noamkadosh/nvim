d05d29b952344ae02471e4d2fa014d217de4db5b
noam <noamkadosh91@gmail.com>
2023-07-31 09:12:38 -0700

    migrate multi cursor plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -167,9 +167,43 @@ return {
     },
 
     {
-        "mg979/vim-visual-multi",
-        branch = "master",
+        "smoka7/multicursors.nvim",
         event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "smoka7/hydra.nvim",
+        },
+        opts = function()
+            local N = require("multicursors.normal_mode")
+            local I = require("multicursors.insert_mode")
+            return {
+                normal_keys = {
+                    -- to change default lhs of key mapping change the key
+                    [","] = {
+                        -- assigning nil to method exits from multi cursor mode
+                        method = N.clear_others,
+                        -- you can pass :map-arguments here
+                        opts = { desc = "Clear others" },
+                    },
+                },
+                insert_keys = {
+                    -- to change default lhs of key mapping change the key
+                    ["<CR>"] = {
+                        -- assigning nil to method exits from multi cursor mode
+                        method = I.Cr_method,
+                        -- you can pass :map-arguments here
+                        opts = { desc = "New line" },
+                    },
+                },
+            }
+        end,
+        keys = {
+            {
+                "<Leader>m",
+                "<cmd>MCstart<cr>",
+                desc = "Create a selection for word under the cursor",
+            },
+        },
     },
 
     {
@@ -202,16 +236,26 @@ return {
                     desc = "Document diagnostics",
                 }
             )
-            vim.keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>", {
-                silent = true,
-                noremap = true,
-                desc = "Location list",
-            })
-            vim.keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>", {
-                silent = true,
-                noremap = true,
-                desc = "Quickfix list",
-            })
+            vim.keymap.set(
+                "n",
+                "<leader>tl",
+                "<cmd>TroubleToggle loclist<cr>",
+                {
+                    silent = true,
+                    noremap = true,
+                    desc = "Location list",
+                }
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>tq",
+                "<cmd>TroubleToggle quickfix<cr>",
+                {
+                    silent = true,
+                    noremap = true,
+                    desc = "Quickfix list",
+                }
+            )
             vim.keymap.set(
                 "n",
                 "<leader>tR",
