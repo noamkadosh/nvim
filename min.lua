fd581f0ee974ce6be55bd480ace0434b516b1c39
noam <noamkadosh91@gmail.com>
2023-11-22 21:43:55 -0800

    fix minimum config

--- min.lua
+++ min.lua
@@ -7,6 +7,7 @@ local options = require("options")
 local theme = require("plugins.theme")
 local treesitter = require("plugins.treesitter")
 local ui = require("plugins.ui")
+local ai = require("plugins.ai")
 local cmp = require("plugins.completion")
 
 require("lazy").setup({
@@ -15,5 +16,6 @@ require("lazy").setup({
     ui,
     { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
     { "tzachar/fuzzy.nvim" },
+    ai,
     cmp,
 }, options)
