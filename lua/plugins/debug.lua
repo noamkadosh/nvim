6a43516b3b323ab1852500d14084fa407c910b3c
noam <noamkadosh91@gmail.com>
2024-05-07 21:34:10 -0700

    feat: update plugins

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -212,6 +212,9 @@ return {
     {
         "rcarriga/nvim-dap-ui",
         lazy = true,
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+        },
         keys = function()
             require("dap")
             local dapui = require("dapui")
