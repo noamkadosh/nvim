36ff6183570d4f25b9df1965020cb37d06816968
noam <noamkadosh91@gmail.com>
2024-06-28 12:46:25 -0700

    feat: don't tint floats

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -388,6 +388,14 @@ return {
         event = { "BufReadPost", "BufNewFile" },
         opts = {
             tint = -60,
+            window_ignore_function = function(winid)
+                local bufid = vim.api.nvim_win_get_buf(winid)
+                local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+                local floating = vim.api.nvim_win_get_config(winid).relative
+                    ~= ""
+
+                return buftype == "terminal" or floating
+            end,
         },
     },
 
