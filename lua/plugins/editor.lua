b6c9b5a77eb6340860683e08af726a17b2442bfa
noam <noamkadosh91@gmail.com>
2024-10-23 23:39:29 -0700

    feat: customize statuscolumn

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -427,24 +427,27 @@ return {
     },
 
     {
+
         "luukvbaal/statuscol.nvim",
         config = function()
-            -- local builtin = require("statuscol.builtin")
+            local builtin = require("statuscol.builtin")
+
             require("statuscol").setup({
-                -- configuration goes here, for example:
-                -- relculright = true,
-                -- segments = {
-                --   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-                --   {
-                --     sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
-                --     click = "v:lua.ScSa"
-                --   },
-                --   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
-                --   {
-                --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-                --     click = "v:lua.ScSa"
-                --   },
-                -- }
+                segments = {
+                    {
+                        sign = {
+                            name = { ".*" },
+                            namespace = { "diagnostic/signs", "gitsigns" },
+                            maxwidth = 1,
+                            wrap = true,
+                        },
+                    },
+                    {
+                        text = { builtin.lnumfunc },
+                        maxwidth = 2,
+                    },
+                    { text = { builtin.foldfunc, " " } },
+                },
             })
         end,
     },
